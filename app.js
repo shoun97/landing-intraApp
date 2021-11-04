bdfa8a012485b97dd3135bb860597e02b17a7717
Shoun97 <manuelurra2497@gmail.com>
2021-11-03 22:04:20 -0300

    solo falta agregar los textos

---
+++ app.js
@@ -0,0 +1,37 @@
+
+require('dotenv').config()
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const port = process.env.PORT;
+let nombre = 'IntraLabs Ltda. @MIUC/JEHU/JOAB/JEZA'
+
+
+
+hbs.registerPartials(__dirname + '/views/partials', function (err){});
+
+app.set('view engine', 'hbs')
+
+
+// servir contenido estático
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.render('home',{
+       nombre
+   });
+});
+
+app.get('/generic', function (req, res) {
+    /* res.sendFile(__dirname + '/public/generic.html') */
+    res.render('generic');
+})
+
+app.get('*', function(req,res){
+    res.render('home',{
+        nombre
+    });
+})
+app.listen(port, () => {
+    console.log(`Servidor Online`);
+})
